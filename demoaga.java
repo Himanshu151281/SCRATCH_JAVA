4d6b6349e8e686dc3a5b42b40a3e09435d41955b
Himanshu Kumar <himanshu151281@gmail.com>
2024-03-31 19:51:51 +0530

    Again Pattern

---
+++ demoaga.java
@@ -0,0 +1,39 @@
+//Again
+//
+// public class demoaga{
+//     public static void main(String args[]){
+//         System.out.println("Again");
+//     }
+// }
+
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+//Again Input Int
+//
+// import java.util.*;
+// public class demoaga{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         System.out.print("Input = ");
+//         System.out.println(a);
+//     }
+// }
+
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+//Again Pattern
+//
+// import java.util.*;
+// public class demoaga {
+//     public static void main(String args[]) {
+//         for (int i = 1; i <= 5; i++) {
+//             for(int j = 1;j <= 5-i; j++ ){
+//                 System.out.print(" ");
+//             }
+//             for(int k = 1; k<=i; k++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
