05ad670072f2dbfd82530e16e50fc94d38b592ab
Himanshu Kumar <himanshu151281@gmail.com>
2024-04-06 23:54:33 +0530

    basics done

--- clg.java
+++ clg.java
@@ -4,7 +4,7 @@ public class clg{
     public static void main(String[] args){
         for(int i = 5; i >= 0; i--){
             for(int j = 0; j < i; j++){
-                System.out.print(" * ");
+                System.out.print(" basics done");
             }
             System.out.println();
         }
