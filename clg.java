bdf88c6dffcbea6d57976c68649843e8980fb43e
Himanshu Kumar <himanshu151281@gmail.com>
2024-04-06 23:51:45 +0530

    reversecd evenly

--- clg.java
+++ clg.java
@@ -4,9 +4,9 @@ public class clg{
     public static void main(String[] args){
         for(int i = 5; i >= 0; i--){
             for(int j = 0; j < i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
