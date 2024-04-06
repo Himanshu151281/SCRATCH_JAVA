e099d96aa4058e6396467cd36b5767e7e37acd4f
Himanshu Kumar <himanshu151281@gmail.com>
2024-04-06 23:56:50 +0530

    basicodes are moved in alg.java

--- clg.java
+++ clg.java
@@ -1,12 +1,1 @@
-import java.util.*;
 
-public class clg{
-    public static void main(String[] args){
-        for(int i = 5; i >= 0; i--){
-            for(int j = 0; j < i; j++){
-                System.out.print(" basics done");
-            }
-            System.out.println();
-        }
-    }
-}
