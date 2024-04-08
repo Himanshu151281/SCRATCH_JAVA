05c9ed256e531aa880e2df668e13d088d29b434e
Himanshu Kumar <himanshu151281@gmail.com>
2024-04-08 13:12:00 +0530

    trrr

---
+++ clgex/trrr.java
@@ -0,0 +1,12 @@
+package clgex;
+
+public class trrr {
+    public static void main(String[] args) {
+        try {
+            // Code that may throw an exception
+        } catch (Exception e) {
+            // Code to handle the exception
+        }
+    }
+}
+
